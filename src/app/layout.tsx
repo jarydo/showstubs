a62285f478d81b4cd2681c16366737a30e00abb5
jarydo <jarydnoahdiamond@gmail.com>
2025-07-06 22:20:04 -0700

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { applause, liebeheide, stampete } from "@/lib/fonts";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`${applause.variable} ${liebeheide.variable} ${stampete.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
