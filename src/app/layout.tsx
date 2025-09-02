c1ecd7e2f27ea213ea4b619dff360b2e89a70fb2
jarydo <jarydnoahdiamond@gmail.com>
2025-09-01 21:57:59 -0400

    third times the charm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
   description: "Generate ticket stubs from your concert history",
   icons: {
     icon: [
-      { url: favicon.src, sizes: '16x16', type: 'image/x-icon' },
-      { url: favicon.src, sizes: '32x32', type: 'image/x-icon' },
+      { url: favicon.src, sizes: "16x16", type: "image/x-icon" },
+      { url: favicon.src, sizes: "32x32", type: "image/x-icon" },
     ],
     apple: favicon.src,
   },
@@ -24,22 +24,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
-          defer
-          src="https://va.vercel-scripts.com/v1/script.debug.js"
-        />
-        <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.va = window.va || function () {
-                (window.vaq = window.vaq || []).push(arguments);
-              };
-              window.va('config', {
-                endpoint: 'https://vitals.vercel-insights.com/v1/vitals',
-                host: 'showstubs-jaryd-diamonds-projects.vercel.app'
-              });
+              window.va =
+                window.va ||
+                function () {
+                  (window.vaq = window.vaq || []).push(arguments);
+                };
             `,
           }}
         />
+        <script
+          defer
+          src="https://showstubs-jaryd-diamonds-projects.vercel.app/_vercel/insights/script.js"
+        ></script>
       </head>
       <body
         className={`${applause.variable} ${liebeheide.variable} ${stampete.variable} antialiased`}
